faa891890d2f50d1cf82e9cc61a5852701927b1e
briology101 <85913910+briology101@users.noreply.github.com>
2022-01-07 21:00:20 -0500

    Initial Project Commit

---
+++ src/Dictionary.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
+import Photos from "./Photos";
+
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState (null);
+
+  function handleDictionaryResponse(response) {
+    setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse (response) {
+      console.log(response);
+      setPhotos(response.data.photos);
+  }
+
+  function search() {
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey = "563492ad6f917000010000017dd1bd62418747c28ee34f1ecedb6681"
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`; 
+    let headers = { Authorization: `Bearer ${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+     
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1> Enter word here</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+              autoFocus={true}
+            />
+          </form>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos}/>
+      </div>
+    );
+  } else {
+    load();
+    return "Digging through the IoT...";
+  }
+}
